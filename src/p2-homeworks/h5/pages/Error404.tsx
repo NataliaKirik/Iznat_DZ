0aea71e14b56374e73d32c6504d22576ba462114
Natalia K <nataliaandkirik@gmail.com>
2021-01-11 02:56:51 +0300

    refactor:styles

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -6,6 +6,8 @@ import s from './Error404.module.css'
 function Error404() {
     return (
         <div className={s.wrapper}>
+            <div className={s.image}><img src="https://cdn.lifedaily.com/assets/7ada33/dist/images/error/404.svg"
+                                          alt="cat"/></div>
             <div className={s.errorText}>Oops, This page does not exist</div>
             <NavLink to={PATH.PRE_JUNIOR} className={s.back}>go to homepage</NavLink>
         </div>
